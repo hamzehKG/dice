27eff01cf77c8dcc7053ee0cf64c4b3769d22c67
Hamzeh Jaafar <hjaafar@konradgroup.com>
2025-07-11 12:25:33 -0400

    Minor refactor alongside example run

--- src/DiceGameSimulation.java
+++ src/DiceGameSimulation.java
@@ -5,28 +5,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-public class Main {
+public class DiceGameSimulation {
 
-    static final int TOTAL_RUN = 1000;
-
+    static final int SIMULATIONS_TO_RUN = 1000;
     static final int NUM_OF_DICE = 5;
-
 
     /**
      * Runs a single iteration of the dice board game
-     * @param numOfDice - The initial number of dices in the game
+     * @param initalDiceCount - The initial number of dices in the game
      * @return The final score of the game
      */
-    public static int playBoardGame(int numOfDice){
+    public static int playBoardGame(int initalDiceCount){
         int finalScore = 0;
         Random random = new Random();
-        while (numOfDice > 0) {
+        while (initalDiceCount > 0) {
 
             List<Integer> rolls = new ArrayList<>();
             int threeCounter = 0;
 
             // Roll all the dices on the board
-            for (int i = 0; i < numOfDice; i++) {
+            for (int i = 0; i < initalDiceCount; i++) {
                 int currentRoll = random.nextInt(6)+1;
                 rolls.add(currentRoll);
 
@@ -38,13 +36,13 @@ public class Main {
 
             if (threeCounter > 0) {
                 // If there are any 3’s, all the 3’s are taken off the board and a score of 0 is awarded
-                numOfDice -= threeCounter;
+                initalDiceCount -= threeCounter;
             } else {
                 // If there are not any 3’s, the lowest die is taken off the board and the value
                 // of that lowest die is awarded for this roll.
                 int lowestValue = Collections.min(rolls);
                 finalScore += lowestValue;
-                numOfDice -= 1;
+                initalDiceCount -= 1;
             }
 
         }
@@ -59,7 +57,6 @@ public class Main {
      */
     public static void runSimulation(int numOfRuns, int numOfDices) {
         System.out.printf("Number of simulations was %d using %d dice.\n", numOfRuns, numOfDices);
-
         long start = System.currentTimeMillis();
 
         Map<Integer, Integer> scores = new HashMap<>();
@@ -69,12 +66,10 @@ public class Main {
 
             // Increment the score for the game we just played
             scores.merge(score, 1, Integer::sum);
-
         }
 
         long finish = System.currentTimeMillis();
         double timeElapsed = (finish - start) / 1000.0;
-
 
         // Sort and log the final scores alongside their frequencies
         List<Integer> sortedScores = new ArrayList<>(scores.keySet());
@@ -86,11 +81,11 @@ public class Main {
             double proportion = (double) count / numOfRuns;
             System.out.printf("Total %d occurs %.4f occurred %d times.%n", score, proportion, count);
         }
-        System.out.printf("Total simulation took %.3f seconds.%n", timeElapsed);
 
+        System.out.printf("Total simulation took %.3f seconds.%n", timeElapsed);
     }
 
     public static void main(String[] args) {
-        runSimulation(TOTAL_RUN, NUM_OF_DICE);
+        runSimulation(SIMULATIONS_TO_RUN, NUM_OF_DICE);
     }
 }
